75fdf4aaa0f6291f5cd6dd50cd34093c7f39cc70
Sören Beye <github@hypfer.de>
2024-10-23 21:38:26 +0200

    🚫🌊

--- handlers/user/UserHandler.js
+++ handlers/user/UserHandler.js
@@ -1,4 +1,5 @@
 import Handler from "../Handler.js";
+import {sleep} from "../../util/tools.js";
 
 class UserHandler extends Handler {
     /**
@@ -12,6 +13,9 @@ class UserHandler extends Handler {
 
         this.uidWhitelist = options.uidWhitelist;
         this.nonsenseCounter = options.nonsenseCounter;
+        
+        this.antifloodCache = [];
+        this.antifloodTimeout = undefined;
     }
 
     async handleMessage(ctx) {
@@ -41,8 +45,49 @@ class UserHandler extends Handler {
                         this.nonsenseCounter.increment();
                     }
                 }
+                
+                this.antifloodCache.push({id: member.id, chat_id: ctx.chat.id, at: Date.now(), removed: false});
+
+                clearTimeout(this.antifloodTimeout);
+                this.antifloodTimeout = setTimeout(() => {
+                    this.evaluateAntiflood(ctx).catch(e => {
+                        console.error("Antiflood failed", e);
+                    });
+                }, 30_000);
             }
         }
+    }
+    
+    async evaluateAntiflood(ctx) {
+        const now = Date.now();
+        
+        this.antifloodCache = this.antifloodCache.filter(e => e.at > (now - 5*60*1000));
+        
+        const membersPerGroup = {};
+        this.antifloodCache.forEach(e => {
+            if (!membersPerGroup[e.chat_id]) {
+                membersPerGroup[e.chat_id] = [];
+            }
+            
+            membersPerGroup[e.chat_id].push(e);
+        });
+        
+        for (const groupMembers of Object.values(membersPerGroup)) {
+            if (groupMembers.length >= 10) {
+                for (const botUser of groupMembers) {
+                    await ctx.tg.banChatMember(
+                        botUser.chat_id,
+                        botUser.id,
+                        Math.floor(now/1000) + 60 // Use an expiring ban to not permaban false positives
+                    );
+
+                    botUser.removed = true;
+                    await sleep(300);
+                }
+            }
+        }
+        
+        this.antifloodCache = this.antifloodCache.filter(e => e.removed !== true);
     }
 }
 
